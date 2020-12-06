becfd388ce640f6a3988fdc9c06bfa3791f1f1e1
Zeki SERT <dijital.donusum@yahoo.com>
2020-12-06 13:43:14 +0300

    Variables Examples

---
+++ 1-JavaScript Basics/2-Types And Variables/6-Variable Declaration and Assigment/index.js
@@ -0,0 +1,75 @@
+/**
+ * EXAMPLE 1 - Variable declaration
+
+
+var a;
+
+console.log(a);
+
+// console.log(b);
+
+
+let c;
+
+console.log(c);
+
+// const d; Uncaught SyntaxError: Missing initializer in const declaration
+
+*/
+
+
+/**
+ * EXAMPLE 2 - "let"
+
+
+let myNumber = 10;
+console.log(myNumber);
+
+
+//declaration
+let myString;
+//console.log(myString); undefined
+//assigment
+myString = "Hello from String";
+console.log(myString);
+//re-assignment
+myString = "New value for myString variable";
+
+console.log(myString);
+*/
+
+/**
+ * EXAMPLE 3 - "var"
+ * alt+click
+
+
+var myNumber = 10;
+console.log(myNumber);
+
+
+//declaration
+var myString;
+//console.log(myString); undefined
+//assigment
+myString = "Hello from String";
+console.log(myString);
+//re-assignment
+myString = "New value for myString variable";
+
+console.log(myString);
+*/
+
+/*
+* EXAMPLE 4 - "const"
+
+const myBoolean = true;
+console.log(myBoolean);
+// Hata alırız !!! myBoolean = false;
+// Hata alırız !!! const myBoolean = false;
+// Hata alırız !!! console.log(myBoolean);
+*/
+
+
+
+
+
